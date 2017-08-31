c1d646f51ecc08c301d24704b9fd0be48be2cb49
miocr <omihula@post.cz>
2017-08-31 11:03:23 +0200

    2

--- Program.cs
+++ Program.cs
@@ -13,19 +13,27 @@ namespace ContosoUniversity
   {
     public static void Main(string[] args)
     {
+#region Net.Core 1
       // var host = new WebHostBuilder()
       //     .UseKestrel()
       //     .UseContentRoot(Directory.GetCurrentDirectory())
       //     .UseIISIntegration()
       //     .UseStartup<Startup>()
       //     .Build();
-
       // host.Run();
+#endregion
+
+#region Net.Core 2
       BuildWebHost(args).Run();
+      //note
+      //note
+      int i = 0;
+#endregion
+
     }
 
     public static IWebHost BuildWebHost(string[] args) =>
-   WebHost.CreateDefaultBuilder(args)
+        WebHost.CreateDefaultBuilder(args)
        .UseStartup<Startup>()
        .Build();
   }
